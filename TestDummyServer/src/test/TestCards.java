390643c9b6440dcfdd6341a3eef983ceb123fff1
Humaira Orchee <orche22h@mtholyoke.edu>
2015-03-02 09:43:56 -0500

    last two tricks left

--- TestDummyServer/src/test/TestCards.java
+++ TestDummyServer/src/test/TestCards.java
@@ -78,26 +78,109 @@ public class TestCards {
 	 * 
 	 */
 	public TestCards(){
+		// Refer to http://www.rpbridge.net/1a00.htm
 		
-		// first 13 are blind pperson's cards
-		cards[0] = ACE_SPADES ;
-		cards[1] = TWO_SPADES ;
+		// Bid is 4 Hearts.
+		
+		// first 13 are blind person's cards. Remember to make blind East
+		cards[0] = EIGHT_SPADES ;
+		cards[1] = FIVE_SPADES ;
 		cards[2] = THREE_SPADES ;
-		cards[3] = FOUR_CLUBS ;
-		cards[4] = FIVE_CLUBS ;
-		cards[5] = SIX_CLUBS ;
-		cards[6] = SEVEN_DIAMONDS ;
-		cards[7] = EIGHT_DIAMONDS ;
+		cards[3] = TWO_SPADES ;
+		cards[4] = JACK_CLUBS ;
+		cards[5] = FIVE_CLUBS ;
+		cards[6] = KING_CLUBS ;
+		cards[7] = TEN_DIAMONDS ;
 		cards[8] = NINE_DIAMONDS ;
-		cards[9] = TEN_HEARTS ;
-		cards[10] = JACK_HEARTS ;
-		cards[11] = QUEEN_HEARTS ;
-		cards[12] = KING_HEARTS ;
+		cards[9] = THREE_DIAMONDS ;
+		cards[10] = EIGHT_DIAMONDS ;
+		cards[11] = FOUR_HEARTS ;
+		cards[12] = SEVEN_HEARTS ;
 		
-		// blind player is not dummy or first player.
-		// trick 1
-		cards[13] = ACE_CLUBS ; 
+		//South is declarer. North is dummy. 
+		
+		// TRICK 1. West plays first.
+		
+		cards[13] = KING_SPADES ; // west
+		cards[14] = FOUR_SPADES ; // north
+		cards[15] = TWO_SPADES ; // east
+		cards[16] =  JACK_SPADES ; // south
+		
+		//  WEST WINS
+		
+		// TRICK 2. West plays first.
+		
+		cards[17] =  ACE_SPADES ; // west
+		cards[18] = SEVEN_SPADES ; // north
+		cards[19] = THREE_SPADES ; // east
+		cards[20] =  FIVE_HEARTS ; // south
+		
+		// SOUTH WINS
+		
+		// TRICK 3. SOUTH plays first
+		cards[21] = KING_HEARTS ; // south
+		cards[22] = TWO_HEARTS ; // west
+		cards[23] = SIX_HEARTS ; // north
+		cards[24] = FOUR_HEARTS ; // east
+		
+		// SOUTH WINS
+		
+		// TRICK 4. SOUTH plays first
+		cards[25] = ACE_HEARTS ; // south
+		cards[26] = THREE_HEARTS ;// west
+		cards[27] = EIGHT_HEARTS ; // north
+		cards[28] = SEVEN_HEARTS ; // east
+		
+		// SOUTH WINS
+		
+		// TRICK 5. SOUTH plays first
+		cards[29] = KING_DIAMONDS ; // south
+		cards[30] = SEVEN_DIAMONDS ; // west
+		cards[31] = TWO_DIAMONDS ; // north
+		cards[32] = THREE_DIAMONDS ; // east
+		
+		// South wins
+		
+		// TRICK 6. South plays first
+		cards[33] = ACE_DIAMONDS ; // south
+		cards[34] = JACK_DIAMONDS ; // west
+		cards[35] = FIVE_DIAMONDS ; // north
+		cards[36] = EIGHT_DIAMONDS ; // east
+		
+		// South wins
+		
+		// Trick 7. South plays first
+		cards[37] = FOUR_DIAMONDS ; // south
+		cards[38] = QUEEN_DIAMONDS ; // west
+		cards[39] = TEN_HEARTS ; // north
+		cards[40] = NINE_HEARTS ; // east
+		// North wins
+		
+		// Trick 8. North plays first
+		cards[41] = TEN_SPADES ; // north
+		cards[42] = FIVE_SPADES ; // east
+		cards[43] = NINE_HEARTS ; // south
+		cards[44] = SIX_HEARTS ; // west
+		
+		// South wins
+		
+		// Trick 9. South plays first
+		cards[45] = SIX_DIAMONDS ; // south
+		cards[46] = NINE_SPADES ; // west
+		cards[47] = JACK_HEARTS ; // north
+		cards[48] = TEN_HEARTS ; //east
+		
+		// north wins
+		
+		// Trick 10. North goes first
+		cards[49] = ACE_CLUBS ; // north
+		cards[50] = FIVE_CLUBS ; // east 
+		cards[51] = FOUR_CLUBS ; // south
+		cards[52] = EIGHT_CLUBS ; // west
 				
+		// north wins
+		
+		// Trick 11. North goes first
 	}
 	
 
